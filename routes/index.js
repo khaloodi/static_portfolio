e86d31b3b3a64599d3f526fdaa85582db8e90c8b
Khaled Adad <khaledadad@lewisu.edu>
2021-05-20 10:50:32 -0500

    profile image displaying

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router() // a router is like a mini app in express, you can write middleware and routes to it
 
-router.use(express.static('public'))
-
 // root route
 router.get('/', (req, res) => {
     res.render('index')
