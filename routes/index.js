210ed1e51a46030074dfc1c8fe9cfad71b983444
Khaled Adad <khaledadad@lewisu.edu>
2021-05-21 08:41:04 -0500

    project page is displaying and routes kind of work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router() // a router is like a mini app in express, you can write middleware and routes to it
+const { projects } = require("../data.json")
 
 // root route
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { projects })
 });
 
 module.exports = router
--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router() // a router is like a mini app in express, you can write middleware and routes to it
+const { projects } = require("../data.json")
 
 // project page
-router.get('/', (req, res) => {
-    res.render('project')
+router.get('/:id', (req, res) => {
+    const projectId = req.params.id
+    res.render('project', { project: projects[projectId] })
 });
 
 module.exports = router
